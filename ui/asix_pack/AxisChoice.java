0e580257ff6240a7bc5381f20e1233fb7165a6ab
marcobonazzi-lgtm <marco.bonazzi@studenti.unisalento.it>
2025-11-29 13:29:24 +0100

    Delete ui directory

--- ui/asix_pack/AxisChoice.java
+++
@@ -1,8 +0,0 @@
-package org.simulator.ui.asix_pack;
-
-public class AxisChoice {
-    public String label;
-    public boolean useDist;
-    public boolean useLapTime;
-    public boolean useAbsTime;
-}
