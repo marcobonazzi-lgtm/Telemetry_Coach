5e093c8091f00d4a4568649cc836b320e7079c1c
marcobonazzi-lgtm <marco.bonazzi@studenti.unisalento.it>
2025-11-29 13:40:44 +0100

    Delete importCSVFW directory

--- importCSVFW/converter/ConverterProfile.java
+++
@@ -1,9 +0,0 @@
-package org.simulator.importCSVFW.converter;
-// ConverterProfile.java
-public enum ConverterProfile {
-    LMU_MOTEC,
-    ACC_MOTEC,             // <— NUOVO
-    ASSETTO_CORSA_PASS_THROUGH,
-    GENERIC_PASS_THROUGH
-}
-
